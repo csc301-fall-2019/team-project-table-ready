e25f2a6ed7ff66bb33e41f0db1b8104013b23aae
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-11-06 16:46:13 -0500

    Merge branch 'admin' of https://github.com/csc301-fall-2019/team-project-table-ready into admin

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Route, Switch, BrowserRouter} from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import './App.scss';
+import SignIn from "./Components/SignIn&Up/signIn"
+import SignUp from "./Components/SignIn&Up/signup"
 import RestaurateurPage from "./Components/Restaurateur/RestaurateurPage";
 import RestaurateurPage2 from "./Components/Restaurateur/RestaurateurPage2";
 import Dashboard from "./Components/Restaurateur/Dashboard";
@@ -10,6 +13,22 @@ import Admin from "./Components/Admin/Admin";
 import "./App.scss";
 
 function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={RestaurateurPage} />
+          <Route exact path="/SignIn" component={SignIn}/>
+          <Route exact path="/SignUp" component={SignUp}/>
+          <Route exact path="/restaurateur" component={RestaurateurPage} />
+          <Route exact path="/restaurateur2" component={RestaurateurPage2} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+      <Customers />
+    </div>
+  );
     return (
         <div className="App">
             <BrowserRouter>
