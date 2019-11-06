982fb2f5d657fb35ea5abccba7b5e5e847af44e6
Xu Wang <jasonwang20150128@gmail.com>
2019-11-05 23:53:51 -0500

    added admin directory

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import {Route, Switch, BrowserRouter} from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import RestaurateurPage from "./Components/Restaurateur/RestaurateurPage";
 import RestaurateurPage2 from "./Components/Restaurateur/RestaurateurPage2";
 import Dashboard from "./Components/Restaurateur/Dashboard";
+import Admin from "./Components/Admin/Admin";
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/restaurateur" component={RestaurateurPage} />
-          <Route exact path="/restaurateur2" component={RestaurateurPage2} />
-          <Route exact path="/dashboard" component={Dashboard} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar/>
+                <Switch>
+                    <Route exact path="/restaurateur" component={RestaurateurPage}/>
+                    <Route exact path="/restaurateur2" component={RestaurateurPage2}/>
+                    <Route exact path="/dashboard" component={Dashboard}/>
+                    <Route exact path="/admin" component={Admin}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
