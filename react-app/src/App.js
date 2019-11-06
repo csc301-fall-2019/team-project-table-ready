a1e70a48a3dbae11023bb38eb8a52db49e309e22
Xiaohe Gong <38306135+xiaohegong@users.noreply.github.com>
2019-11-06 16:24:02 -0500

    Revert "Set up server and MongoDB"

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,7 +10,6 @@ import RestaurateurPage from "./Components/Restaurateur/RestaurateurPage";
 import RestaurateurPage2 from "./Components/Restaurateur/RestaurateurPage2";
 import Dashboard from "./Components/Restaurateur/Dashboard";
 import "./App.scss";
-import Customers from "./Components/Customers";
 
 function App() {
   return (
@@ -26,7 +25,6 @@ function App() {
           <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
       </BrowserRouter>
-      <Customers />
     </div>
   );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const mongoose = require('./mongoose');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
