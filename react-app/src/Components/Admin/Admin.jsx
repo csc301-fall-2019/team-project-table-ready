53b60ee067d73b4c4cd2d01fd71ead6985f17ec8
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-11-07 12:41:35 -0500

    working on 'overview' of admin page

--- react-app/src/Components/Admin/Admin.jsx
+++ react-app/src/Components/Admin/Admin.jsx
@@ -1,13 +1,55 @@
 import React, {Component} from "react";
-import RestaurantListItem from "../Restaurateur/RestaurantListItem";
+import "../../Stylesheets/admin_page.scss";
+
 
 class Admin extends Component {
   state = {};
 
+  chooseOverview = () => {
+
+  };
+
+  chooseManage = () => {
+
+  };
+
+  chooseSetting = () => {
+
+  };
+
+  showContent = () => {
+
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello World!!</h1>
+      <div className='admin-page'>
+        <div className="row menu-bar">
+          <div className="col-sm-8 menu">
+            <div className="btn-group btn-group-lg button-group" role="group" aria-label="Large button group">
+              <button type="button" className="btn btn-secondary btn-danger btn-outline-dark menu-button"
+                      onClick={this.chooseOverview}>Overview
+              </button>
+              <button type="button" className="btn btn-secondary btn-danger btn-outline-dark menu-button"
+                      onClick={this.chooseManage}>Manage
+              </button>
+              <button type="button" className="btn btn-secondary btn-danger btn-outline-dark menu-button"
+                      onClick={this.chooseSetting}>Setting
+              </button>
+            </div>
+          </div>
+          <div className="col-sm-4 profile">
+            <div className="user-info">
+              <div className="row">
+                <div className="">
+                  <img className="avatar" src='../../../images/avatar_sample.png' alt=""/>
+                  <strong>Welcome, Juliet!</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="admin-content">{this.showContent}</div>
       </div>
     );
   }
