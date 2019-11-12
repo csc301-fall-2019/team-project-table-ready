03e6200366ed3019c27087431c40ad3f1c8b6c3f
Lantao Cui <clt2738461982@gmail.com>
2019-11-12 18:58:12 -0500

    dont touch this commit

--- react-app/src/Components/Restaurateur/RestaurateurPage.jsx
+++ react-app/src/Components/Restaurateur/RestaurateurPage.jsx
@@ -20,102 +20,116 @@ class RestaurateurPage extends Component {
               console.log(400);
           });
   }
-  
+  is_authenticated = () => {
+    const cur_user = this.props.cookies.cookies.cur_user
+    if (cur_user.accountType != "Employee"){
+      return true
+    }
+    return false
+  }
   render() {
     console.log(this.props.cookies.cookies);
-    return (
-      <div className="restaurateur-page">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3 info">
-              <h2 className="">Your Name</h2>
-              <div>
-                <img
-                  src={process.env.PUBLIC_URL + "/images/avatar_sample.png"}
-                  alt=""
-                  className="avatar"
-                />
+    if (this.is_authenticated()){
+      return (
+        <div className="restaurateur-page">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3 info">
+                <h2 className="">Your Name</h2>
+                <div>
+                  <img
+                    src={process.env.PUBLIC_URL + "/images/avatar_sample.png"}
+                    alt=""
+                    className="avatar"
+                  />
+                </div>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    <strong>Email: </strong>name@email.com
+                  </li>
+                  <li className="list-group-item">
+                    <strong>Telephone: </strong>123-456-7890
+                  </li>
+                  <li className="list-group-item">
+                    <strong>Address: </strong>788 Creek Lane Simpsonville, SC
+                    29680
+                  </li>
+                </ul>
               </div>
-              <ul className="list-group">
-                <li className="list-group-item">
-                  <strong>Email: </strong>name@email.com
-                </li>
-                <li className="list-group-item">
-                  <strong>Telephone: </strong>123-456-7890
-                </li>
-                <li className="list-group-item">
-                  <strong>Address: </strong>788 Creek Lane Simpsonville, SC
-                  29680
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-md-9">
-              <h2 style={{display:'inline'}}>Your Restaurants</h2>
-              <Link to="/addNewRestaurant"><button className={"addNewButton"}> Add New </button></Link>
-              <div className="restaurants-display">
-                <div className="list-group">
-                  <RestaurantListItem
-                    name="Restaurant Name"
-                    address="788 Creek Lane Simpsonville, SC 29680"
-                    telephone="123-456-7890"
-                    image={
-                      process.env.PUBLIC_URL +
-                      "/images/restaurant_images/restaurant1.jpeg"
-                    }
-                  />
-                  <RestaurantListItem
-                    name="Restaurant Name"
-                    address="788 Creek Lane Simpsonville, SC 29680"
-                    telephone="123-456-7890"
-                    image={
-                      process.env.PUBLIC_URL +
-                      "/images/restaurant_images/restaurant2.jpeg"
-                    }
-                  />
-                  <RestaurantListItem
-                    name="Restaurant Name"
-                    address="788 Creek Lane Simpsonville, SC 29680"
-                    telephone="123-456-7890"
-                    image={
-                      process.env.PUBLIC_URL +
-                      "/images/restaurant_images/restaurant3.jpeg"
-                    }
-                  />
-                  <RestaurantListItem
-                    name="Restaurant Name"
-                    address="788 Creek Lane Simpsonville, SC 29680"
-                    telephone="123-456-7890"
-                    image={
-                      process.env.PUBLIC_URL +
-                      "/images/restaurant_images/restaurant1.jpeg"
-                    }
-                  />
-                  <RestaurantListItem
-                    name="Restaurant Name"
-                    address="788 Creek Lane Simpsonville, SC 29680"
-                    telephone="123-456-7890"
-                    image={
-                      process.env.PUBLIC_URL +
-                      "/images/restaurant_images/restaurant2.jpeg"
-                    }
-                  />
-                  <RestaurantListItem
-                    name="Restaurant Name"
-                    address="788 Creek Lane Simpsonville, SC 29680"
-                    telephone="123-456-7890"
-                    image={
-                      process.env.PUBLIC_URL +
-                      "/images/restaurant_images/restaurant3.jpeg"
-                    }
-                  />
+  
+              <div className="col-md-9">
+                <h2 style={{display:'inline'}}>Your Restaurants</h2>
+                <Link to="/addNewRestaurant"><button className={"addNewButton"}> Add New </button></Link>
+                <div className="restaurants-display">
+                  <div className="list-group">
+                    <RestaurantListItem
+                      name="Restaurant Name"
+                      address="788 Creek Lane Simpsonville, SC 29680"
+                      telephone="123-456-7890"
+                      image={
+                        process.env.PUBLIC_URL +
+                        "/images/restaurant_images/restaurant1.jpeg"
+                      }
+                    />
+                    <RestaurantListItem
+                      name="Restaurant Name"
+                      address="788 Creek Lane Simpsonville, SC 29680"
+                      telephone="123-456-7890"
+                      image={
+                        process.env.PUBLIC_URL +
+                        "/images/restaurant_images/restaurant2.jpeg"
+                      }
+                    />
+                    <RestaurantListItem
+                      name="Restaurant Name"
+                      address="788 Creek Lane Simpsonville, SC 29680"
+                      telephone="123-456-7890"
+                      image={
+                        process.env.PUBLIC_URL +
+                        "/images/restaurant_images/restaurant3.jpeg"
+                      }
+                    />
+                    <RestaurantListItem
+                      name="Restaurant Name"
+                      address="788 Creek Lane Simpsonville, SC 29680"
+                      telephone="123-456-7890"
+                      image={
+                        process.env.PUBLIC_URL +
+                        "/images/restaurant_images/restaurant1.jpeg"
+                      }
+                    />
+                    <RestaurantListItem
+                      name="Restaurant Name"
+                      address="788 Creek Lane Simpsonville, SC 29680"
+                      telephone="123-456-7890"
+                      image={
+                        process.env.PUBLIC_URL +
+                        "/images/restaurant_images/restaurant2.jpeg"
+                      }
+                    />
+                    <RestaurantListItem
+                      name="Restaurant Name"
+                      address="788 Creek Lane Simpsonville, SC 29680"
+                      telephone="123-456-7890"
+                      image={
+                        process.env.PUBLIC_URL +
+                        "/images/restaurant_images/restaurant3.jpeg"
+                      }
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else{
+      return (
+        <Redirect to = "/error"></Redirect>
+      )
+    }
+  
   }
 }
 
